eabcf1a0936fd0e05d8bf3428305b50d4c7ff0f1
LMJW <heysuperming@gmail.com>
2017-12-29 13:09:17 +1100

    test of loading dictionary as function kwargs

--- app/client.py
+++ app/client.py
@@ -3,12 +3,13 @@ import grpc
 import test_pb2
 import test_pb2_grpc
 
-def run():
-    channel = grpc.insecure_channel('server:22222')
-    stub = test_pb2_grpc.EncodeServiceStub(channel)
-    response = stub.GetEncode(test_pb2.plaintext(pttransactionID = 'abcde',
+x = dict(pttransactionID = 'abcde',
     ptproperties = 'This is a plain text transaction',
-    ptsenderID = 'Will smith'))
+    ptsenderID = 'Will smith')
+def run():
+    channel = grpc.insecure_channel('localhost:22222')
+    stub = test_pb2_grpc.EncodeServiceStub(channel)
+    response = stub.GetEncode(test_pb2.plaintext(**x))
     print("Encdded service received:\n EnctransactionID:%s\n,Encproperties:%s\n,EncsenderID:%s\n"%(response.enctransactionID,response.encproperties,response.encsenderID))
 
 if __name__ == "__main__":
